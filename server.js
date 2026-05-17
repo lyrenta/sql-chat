662b7b71fff08a506d27fce3338fc5aa67871136
Lataj2011 <latajdmitro2011@gmail.com>
2026-05-17 17:29:17 +0300

    hello

--- server.js
+++ server.js
@@ -1,30 +1,187 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const mysql = require("mysql2");
 
-const pathToIndex = path.join(__dirname, 'static', 'index.html');
-const indexHtmlFile = fs.readFileSync(pathToIndex);
 
-const pathToStyle = path.join(__dirname, 'static', 'style.css');
-const styleCssFile = fs.readFileSync(pathToStyle);
-
-const pathToScript = path.join(__dirname, 'static', 'script.js');
-const scriptJsFile = fs.readFileSync(pathToScript);
-
-const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        return res.end(indexHtmlFile);
-    }
-    if(req.url === '/style.css') {
-        res.setHeader('Content-Type', 'text/css');
-        return res.end(styleCssFile);
-    }
-    if(req.url === '/script.js') {
-        res.setHeader('Content-Type', 'application/javascript');
-        return res.end(scriptJsFile);
-    }
-    res.statusCode = 404;
-    return res.end('error 404')
+// MYSQL CONNECTION
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "chat"
 });
 
-server.listen(3000);
+// CONNECT MYSQL
+connection.connect((err) => {
+
+    if (err) {
+        console.log("Database error:", err);
+    } else {
+        console.log("MySQL connected!");
+    }
+});
+
+
+// SERVER
+const server = http.createServer((req, res) => {
+
+    // INDEX.HTML
+    if (req.url === '/') {
+
+        const file = fs.readFileSync(
+            path.join(__dirname, 'static', 'index.html')
+        );
+
+        res.setHeader('Content-Type', 'text/html');
+
+        return res.end(file);
+    }
+
+    // STYLE.CSS
+    if (req.url === '/style.css') {
+
+        const file = fs.readFileSync(
+            path.join(__dirname, 'static', 'style.css')
+        );
+
+        res.setHeader('Content-Type', 'text/css');
+
+        return res.end(file);
+    }
+
+    // SCRIPT.JS
+    if (req.url === '/script.js') {
+
+        const file = fs.readFileSync(
+            path.join(__dirname, 'static', 'script.js')
+        );
+
+        res.setHeader('Content-Type', 'application/javascript');
+
+        return res.end(file);
+    }
+
+    // GET MESSAGES
+    if (req.url === "/messages" && req.method === "GET") {
+
+        connection.query(
+            "SELECT * FROM message ORDER BY id ASC",
+            (err, results) => {
+
+                if (err) {
+
+                    console.log("MYSQL ERROR:", err);
+
+                    res.writeHead(500, {
+                        "Content-Type": "application/json"
+                    });
+
+                    return res.end(JSON.stringify({
+                        error: err.message
+                    }));
+                }
+
+                res.writeHead(200, {
+                    "Content-Type": "application/json"
+                });
+
+                return res.end(JSON.stringify(results));
+            }
+        );
+
+        return;
+    }
+
+    // SEND MESSAGE
+    if (req.url === "/send" && req.method === "POST") {
+
+        let body = "";
+
+        req.on("data", chunk => {
+            body += chunk.toString();
+        });
+
+        req.on("end", () => {
+
+            const data = JSON.parse(body);
+
+            connection.query(
+                "INSERT INTO message (username, message) VALUES (?, ?)",
+                [data.user, data.message],
+                (err) => {
+
+                    if (err) {
+
+                        console.log("MYSQL ERROR:", err);
+
+                        res.writeHead(500);
+
+                        return res.end("Database error");
+                    }
+
+                    res.writeHead(200);
+
+                    return res.end("Message saved!");
+                }
+            );
+        });
+
+        return;
+    }
+
+    // REGISTER
+    if (req.url === "/register" && req.method === "POST") {
+
+        let body = "";
+
+        req.on("data", chunk => {
+            body += chunk.toString();
+        });
+
+        req.on("end", () => {
+
+            const data = JSON.parse(body);
+
+            connection.query(
+                "INSERT INTO user (user, password) VALUES (?, ?)",
+                [data.user, data.password],
+                (err) => {
+
+                    if (err) {
+
+                        console.log("MYSQL ERROR:", err);
+
+                        res.writeHead(500);
+
+                        return res.end("Register error");
+                    }
+
+                    res.writeHead(200);
+
+                    return res.end("User created!");
+                }
+            );
+        });
+
+        return;
+    }
+
+    // 404
+    res.writeHead(404);
+
+    res.end("404 Not Found");
+});
+
+// START SERVER
+server.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', async (socket) => {
+    const guestNickname = 'gist_' + Math.floor(Math.random() * 1000);
+    console.log(`${guestNickname} pidkluchivsya. id - ${socket.io}`);
+})
--- static/script.js
+++ static/script.js
@@ -1,1 +1,89 @@
-alert('welcome to my chat!')
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const messagesDiv = document.querySelector(".messagesFromYou");
+const socket = io();
+
+// SEND MESSAGE
+button.addEventListener("click", async () => {
+
+    const message = input.value;
+
+    if (message.trim() === "") return;
+
+    await fetch("/send", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: "You",
+            message: message
+        })
+    });
+
+    input.value = "";
+
+    loadMessages();
+});
+
+// LOAD MESSAGES
+async function loadMessages() {
+
+    try {
+
+        const response = await fetch("/messages");
+
+        if (!response.ok) {
+            throw new Error("Server error");
+        }
+
+        const messages = await response.json();
+
+        messagesDiv.innerHTML = "";
+
+        messages.forEach(msg => {
+
+            const p = document.createElement("p");
+
+            p.textContent = `${msg.username}: ${msg.message}`;
+
+            messagesDiv.appendChild(p);
+        });
+
+    } catch (error) {
+
+        console.log("Error loading messages:", error);
+    }
+}
+
+loadMessages();
+
+setInterval(loadMessages, 1000);
+
+const registerBtn = document.getElementById("registerBtn");
+
+registerBtn.addEventListener("click", async () => {
+
+    const username = document.getElementById("regUsername").value;
+    const password = document.getElementById("regPassword").value;
+
+    if (username === "" || password === "") {
+        alert("Fill all fields");
+        return;
+    }
+
+    const response = await fetch("/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    });
+
+    const text = await response.text();
+
+    alert(text);
+});
